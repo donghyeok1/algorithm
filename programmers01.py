a30ab91d2169854fc47cfadc0af0a1281c553e5c
donghyeok1 <07385@naver.com>
2022-12-09 19:43:18 +0900

    프로그래머스 소수 찾기

--- programmers01.py
+++ programmers01.py
@@ -2,9 +2,7 @@ import math
 
 def solution(k, d):
     answer = 0
-    
     for x in range(0, d + 1, k):
         max_y = int(math.sqrt(math.pow(d, 2) - math.pow(x, 2)))
         answer += max_y // k + 1
-    
     return answer
