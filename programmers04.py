d8e34e5c6961162c7b044c1d8622299348ecccb2
donghyeok1 <07385@naver.com>
2022-12-09 19:42:28 +0900

    프로그래머스 귤 고르기

--- programmers04.py
+++ programmers04.py
@@ -1,1 +1,12 @@
-print("hi")
+from collections import Counter
+def solution(k, tangerine):
+    answer = 0
+    dict_tan = dict(Counter(tangerine))
+    dict_tan = sorted(dict_tan.items(), key = lambda x : x[1], reverse = True)
+    for cnt in dict_tan:
+        if k - cnt[1] <= 0:
+            return answer + 1
+        else:
+            k -= cnt[1]
+            answer += 1
+    return answer
