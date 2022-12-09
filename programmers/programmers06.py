606d25493ea156bb0b2d6fe0097a4f5aed13bc3f
donghyeok1 <07385@naver.com>
2022-12-09 20:42:00 +0900

    프로그래머스 롤케이크 자르기

--- programmers/programmers06.py
+++ programmers/programmers06.py
@@ -1,1 +1,19 @@
-print("hi")
+from collections import Counter
+def solution(topping):
+    answer = 0
+    total_dic = dict(Counter(topping))
+    brother_dic = dict()
+    
+    for i in range(len(topping)):
+        if topping[i] not in brother_dic:
+            brother_dic[topping[i]] = 1
+        
+        total_dic[topping[i]] -= 1
+
+        if total_dic[topping[i]] == 0:
+            del total_dic[topping[i]]
+        
+        if len(brother_dic) == len(total_dic):
+            answer += 1
+        
+    return answer
