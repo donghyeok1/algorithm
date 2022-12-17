7fdafae9f4a71927733d8297d94254dd21729dcb
donghyeok1 <07385@naver.com>
2022-12-18 04:02:45 +0900

    프로그래머스 k진수에서 소수 개수 구하기

--- programmers/programmers14.py
+++ programmers/programmers14.py
@@ -1,1 +1,23 @@
-print("hif")
+def solution(n, k):
+    answer = 0
+    trans = ""
+    while n >= 1:
+        n, b = divmod(n, k)
+        trans += str(b)
+        
+    trans = trans[::-1]
+
+    sosu_ls = []
+    
+    for num in trans.split("0"):
+        if len(num) != 0 and num != "1":
+            sosu_ls.append(int(num))
+
+    for num in sosu_ls:
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0 and i != 2:
+                break
+        else:
+            answer += 1
+
+    return answer
