4ef0e2468af216e76704bb4e71cbc35688153a00
donghyeok1 <07385@naver.com>
2022-12-24 17:21:06 +0900

    프로그래머스 2개 이하로 다른 비트

--- programmers/programmers22.py
+++ programmers/programmers22.py
@@ -1,1 +1,20 @@
-print("hi")
+def solution(numbers):
+    answer = []
+    for i in numbers:
+        if i % 2 == 0:
+            answer.append(i + 1)
+        else:
+            a = str(format(i, 'b'))
+            zero_list = []
+            for j in range(len(a)):
+                if a[j] == "0":
+                    zero_list.append(j)
+            if not zero_list:
+                ans = "10" + a[1:]
+                answer.append(int(ans, 2))
+            else:
+                ans = a[:zero_list[-1]] + "10"
+                if len(a) > zero_list[-1] + 2:
+                    ans += a[zero_list[-1] + 2 :]
+                answer.append(int(ans, 2))
+    return answer
