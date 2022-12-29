7ae73be83a556713caba1cfb19fea8726aa7d5ef
donghyeok1 <07385@naver.com>
2022-12-30 01:54:45 +0900

    프로그래머스 마법의 엘리베이터

--- programmers/programmers28.py
+++ programmers/programmers28.py
@@ -1,1 +1,25 @@
-print("hiㅇ")
+def solution(storey):
+    answer = 0
+    res = int(storey)
+
+    while True:
+        if res == 0:
+            break
+        a = res % 10
+        if a >= 6:
+            answer += 10 - a
+            res = res // 10 + 1
+        elif a == 5:
+            temp = res // 10
+            b = temp % 10
+            if b == 5 or b == 9:
+                answer += 5
+                res = res // 10 + 1
+            else:
+                answer += 5
+                res = res // 10
+        else:
+            answer += a
+            res = res // 10
+
+    return answer
