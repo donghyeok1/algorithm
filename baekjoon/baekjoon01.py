aee9e1e2ddda9dc0b5195267102603ec8da0ba0c
donghyeok1 <07385@naver.com>
2023-01-02 16:20:42 +0900

    백준 AC

--- baekjoon/baekjoon01.py
+++ baekjoon/baekjoon01.py
@@ -1,1 +1,38 @@
-print("hi")
+import sys
+from collections import deque
+
+T = int(sys.stdin.readline())
+
+for i in range(T):
+    command = sys.stdin.readline().rstrip()
+    ls_len = int(sys.stdin.readline())
+    input_ls = sys.stdin.readline().rstrip().split(',')
+    ls = []
+    for w in input_ls:
+        temp = ""
+        for j in w:
+            if j != "[" and j != "," and j != "]":
+                temp += j
+        if temp:
+            ls.append(int(temp))
+    ls = deque(ls)
+    flag = 1 # 1은 정방향, 0은 반대
+    for word in command:
+        if word == "R":
+            if flag:
+                flag = 0
+            else:
+                flag = 1
+        else:
+            if ls:
+                if flag:
+                    ls.popleft()
+                else:
+                    ls.pop()
+            else:
+                print("error")
+                break
+    else:
+        if not flag:
+            ls.reverse()
+        print('['+','.join(map(str, ls))+']')
