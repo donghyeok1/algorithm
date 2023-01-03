f4111e02da0988acc547277c303d2c207591eb2e
donghyeok1 <07385@naver.com>
2023-01-03 11:55:36 +0900

    프로그래머스 튜플

--- programmers/programmers32.py
+++ programmers/programmers32.py
@@ -2,28 +2,21 @@ def solution(s):
     answer = set()
     result = []
     set_ls = []
-    temp = ""
-    temp_ls = set()
     flag = False
     
-    for i in s.split(","):
-        temp = ""
-        for w in i:
-            if w != "{" and w != "}":
-                temp += w
-            if w == "}":
-                flag = True
-        temp_ls.add(int(temp))
-        
-        if flag:
-            set_ls.append((temp_ls))
-            temp_ls = set()
-            flag = False
+    s = s[2 : -2]
     
+    for word_split in s.split("},{"):
+        temp_ls = set()
+        for final_split in word_split.split(","):
+            temp_ls.add(int(final_split))
+        set_ls.append(temp_ls)
+        
     set_ls = sorted(set_ls, key = len)
     
     for tup in set_ls:
         temp = tup - answer
         answer |= temp
         result.append(temp.pop())
+
     return result
