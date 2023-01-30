1dab1bc7f98186ecffcbf279b4010de07d8dc132
donghyeok1 <95459089+donghyeok1@users.noreply.github.com>
2023-01-30 12:24:03 +0900

    [소프티어] 근무 시간

---
+++ Softeer/softeer001.py
@@ -0,0 +1,14 @@
+import sys
+
+total = 0
+for i in range(5):
+    start, end = sys.stdin.readline().split(" ")
+    start_h, start_m = map(int, start.split(":"))
+    end_h, end_m = map(int, end.split(":"))
+
+    if end_m >= start_m:
+        total += (end_h - start_h) * 60 + end_m - start_m
+    else:
+        total += (end_h - start_h - 1) * 60 + end_m + 60 - start_m
+
+print(total)
