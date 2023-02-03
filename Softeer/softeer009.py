66e451396279ddb7809f3c7acd66faed04bd75c9
donghyeok1 <07385@naver.com>
2023-02-03 17:01:04 +0900

    소프티어 슈퍼컴퓨터 클러스터

--- Softeer/softeer009.py
+++ Softeer/softeer009.py
@@ -1,33 +1,30 @@
 import sys
-import heapq
 
 n, b = map(int, sys.stdin.readline().split())
 
 a = list(map(int, sys.stdin.readline().split()))
 
-heap = []
-for i in a:
-    heapq.heappush(heap, (i, 0))
+a = sorted(a)
 
-total = 0
-while True:
-    min_val, update = heapq.heappop(heap)
-    if heap:
-        if min_val <= heap[0][0]:
-            total -= update ** 2
-            if total + (update + 1) ** 2 == b:
-                if min_val ==heap[0][0]:
-                    print(min_val)
-                else:
-                    print(min_val + 1)
-                break
-            elif total + (update + 1) ** 2 < b:
-                total += (update + 1) ** 2
-                heapq.heappush(heap, (min_val + 1, update + 1))
-            else:
-                print(min_val)
-                break
+start = 1
+end = 10 ** 9
 
+while start < end:
+    total = b
+    mid = (start + end) // 2 + 1
+    res = end
+
+    for i in range(len(a)):
+        if mid <= a[i]:
+            break
+        total -= (mid - a[i]) ** 2
+        if total < 0:
+            break
+    if total > 0:
+        start = mid
+    elif total == 0:
+        break
     else:
-        res = b ** 0.5
-        print(min_val + res)
+        end = mid - 1
+
+print((start + end) // 2)
