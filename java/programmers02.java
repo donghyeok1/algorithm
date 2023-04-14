8593115d2a7555720116733800f6c0ded7efa430
donghyeok1 <07385@naver.com>
2023-04-14 17:08:10 +0900

    연속된 수의 합

---
+++ java/programmers02.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int num, int total) {
+        int[] answer = new int[num];
+        int first = 0;
+        if (num % 2 == 0){
+            first = total / num - num / 2 + 1;
+        }
+        else{
+            first = total / num - num / 2;
+        }
+        for (int i = 0; i < num; i++){
+            answer[i] = first + i;
+        }
+        return answer;
+    }
+}
